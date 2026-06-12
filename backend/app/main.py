06f6fdce0511b98debbc5d424a006506e0bd49d4
Khushi <iamxkhushi.1726@gmail.com>
2026-06-12 10:25:44 +0530

    feat: setup PostgreSQL and create users table

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI
 
+from app.db.database import Base
+from app.db.database import engine
+
+from app.models.user import User
+
+print("Creating database tables...")
+
+Base.metadata.create_all(bind=engine)
+
+print("Database tables created.")
+
 app = FastAPI(
     title="Verse Vault API",
     version="1.0.0"
 )
 
+
 @app.get("/")
 def root():
     return {
-        "message": "Welcome to Verse Vault"
+        "message": "Verse Vault API Running"
     }
+
 
 @app.get("/health")
 def health():
